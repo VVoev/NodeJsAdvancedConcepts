53f195a5c90667c69bc031eb75e18c47a7cf1d40
vladovoev <vladovoev@yahoo.co.uk>
2018-05-09 15:20:36 +0300

    redis initial calls

--- AdvancedNodeStarter/routes/blogRoutes.js
+++ AdvancedNodeStarter/routes/blogRoutes.js
@@ -14,9 +14,27 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+    const redis = require('redis');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+    const util = require('util');
+    client.get = util.promisify(client.get);
 
+    //check if data is available in redis
+    const cachedBlogs = await client.get(req.user.id);
+    console.log(cachedBlogs);
+    //if yes respond right away and return
+    if (cachedBlogs) {
+      console.log('Serving from cache')
+      return res.send(JSON.parse(cachedBlogs));
+    }
+    //if not availabe respond to rquest and update redis
+
+    const blogs = await Blog.find({ _user: req.user.id });
+    console.log('Serving from mongodb')
     res.send(blogs);
+
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
