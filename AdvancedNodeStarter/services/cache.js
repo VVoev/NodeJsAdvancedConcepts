aa50941b69d102feed19eab90ab6bf75ec85c00d
vladovoev <vladovoev@yahoo.co.uk>
2018-05-10 13:11:06 +0300

    added redis cache

--- AdvancedNodeStarter/services/cache.js
+++ AdvancedNodeStarter/services/cache.js
@@ -1,19 +1,20 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
 const util = require('util');
+const keys = require('../config/keys');
 
-const redisUrl = 'redis://127.0.0.1:6379';
-const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+const client = redis.createClient(keys.redisUrl);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function () {
+mongoose.Query.prototype.cache = function (options = {}) {
     this.useCache = true;
+    this.hashKey = JSON.stringify(options.key || '');
+
     return this;
-}
+};
 
 mongoose.Query.prototype.exec = async function () {
-
     if (!this.useCache) {
         return exec.apply(this, arguments);
     }
@@ -21,20 +22,31 @@ mongoose.Query.prototype.exec = async function () {
     const key = JSON.stringify(
         Object.assign({}, this.getQuery(), {
             collection: this.mongooseCollection.name
-        }))
+        })
+    );
 
-    //see if we have a value for key
-    const cacheValue = await client.get(key);
-    //if we do return
+    // See if we have a value for 'key' in redis
+    const cacheValue = await client.hget(this.hashKey, key);
+
+    // If we do, return that
     if (cacheValue) {
         const doc = JSON.parse(cacheValue);
-        return Array.isArray(doc) ?
-            doc.map(d => new this.model(d)) :
-            new this.model(doc);
-    }
-    //else issue the query and store the result in redis
-    const result = await exec.apply(this, arguments);
-    client.set(key, JSON.stringify(result));
-    return result;
-}
 
+        return Array.isArray(doc)
+            ? doc.map(d => new this.model(d))
+            : new this.model(doc);
+    }
+
+    // Otherwise, issue the query and store the result in redis
+    const result = await exec.apply(this, arguments);
+
+    client.hset(this.hashKey, key, JSON.stringify(result));
+
+    return result;
+};
+
+module.exports = {
+    clearHash(hashKey) {
+        client.del(JSON.stringify(hashKey));
+    }
+};
