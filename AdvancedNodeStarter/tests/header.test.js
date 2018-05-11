25be798629b0931df733086e2e126b1b1dd29e22
vladovoev <vladovoev@yahoo.co.uk>
2018-05-11 12:52:04 +0300

    making a facade of a pupeteer API

--- AdvancedNodeStarter/tests/header.test.js
+++ AdvancedNodeStarter/tests/header.test.js
@@ -1,5 +1,3 @@
-const sessionFactory = require('./factories/sessionFactory');
-const userFactory = require('./factories/userFactory');
 const Page = require('./helpers/page');
 let page;
 
@@ -14,7 +12,7 @@ afterEach(async () => {
 
 
 test('the header has the correct text', async () => {
-    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+    const text = await page.getContentsOf('a.brand-logo');
     expect(text).toEqual('Blogster');
 })
 
@@ -25,18 +23,9 @@ test('clicking login starts oath flow', async () => {
 })
 
 test('When signed in,shows logout button', async () => {
-    //const id = '5af2cdd99aa73733e8339faf';
+    await page.login();
 
-    const user = await userFactory();
-    const { session, sig } = sessionFactory(user);
+    const text = await page.getContentsOf('a[href="/auth/logout"]')
 
-    await page.setCookie({ name: 'session', value: session });
-    await page.setCookie({ name: 'session.sig', value: sig });
-    await page.goto('localhost:3000');
-
-
-    await page.waitFor('a[href="/auth/logout"]');
-    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
     expect(text).toEqual('Logout');
-
 })
